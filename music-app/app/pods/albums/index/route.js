97efbfb5b8f52a678ad0d52b903df92b6fe49b6c
Mike Lemuel Bacayo <mike.lemuelbacayo@gmail.com>
2020-01-25 04:41:25 +0800

    Route user to albums list

--- music-app/app/pods/albums/index/route.js
+++ music-app/app/pods/albums/index/route.js
@@ -1,6 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    this.transitionTo('albums');
+  },
+
   model() {
     return this.store.findAll('album', {
       include: 'songs'
